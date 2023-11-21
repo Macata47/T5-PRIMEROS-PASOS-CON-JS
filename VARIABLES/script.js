45bf0f9629255b3b7d2a41cba3b7b5f711e53b9d
Marta Parra <149329102+Macata47@users.noreply.github.com>
2023-11-21 14:00:09 +0100

    T5-PRIMEROS-PASOS-CON-JS

--- VARIABLES/script.js
+++ VARIABLES/script.js
@@ -2,14 +2,90 @@
 
 /* VARIABLES */
 
-/* Variables de número */
+/* Crear variables de diferentes tipos de datos: número, cadena de texto, booleano e imprimir su valor por 
+la pantalla del navegador y la consola. */
+
+//Numero.
+
+let numero = 47
+
+console.log(numero);
+document.write(numero);
 
 
-let numero1 = 1
-let numero2 = 3
-let numero3 = 6
+//Cadena de texto.
 
-let suma = numero1 + numero2 + numero3;
+let nombre = "Marta"
 
-console.log(suma)
+console.log(nombre);
+document.write(nombre);
 
+let apellido = "Parra"
+
+console.log(apellido);
+document.write(apellido);
+
+
+
+
+//Booleano.
+
+let x = false;
+
+console.log(x);
+document.write(x);
+
+let green = true;
+
+console.log(green);
+document.write(green);
+
+
+
+
+
+/* De las variables declaradas anteriormente, identifica las que son de tipo integer y realiza operaciones 
+matemáticas básicas con las variables creadas anteriormente (suma, resta, multiplicación, división). 
+Deberás imprimir el resultado por la pantalla del navegador y la consola. */
+
+// Variables de tipo integer.
+
+/* La única variable de tipo integer es la primera (la de número y porque es entero) */
+
+// Suma
+
+let suma = 7 + 4
+console.log(suma);
+document.write(suma);
+
+
+// Resta
+
+let resta = 7 - 4
+console.log(resta);
+document.write(resta);
+
+
+// Multiplicación
+
+let multiplicacion = 7 * 4
+console.log(multiplicacion);
+document.write(multiplicacion);
+
+
+
+// División
+
+let division = 7 / 4
+console.log(division);
+document.write(division);
+
+
+/* De las variables declaradas anteriormente, identifica las que son de tipo texto y concatena dichas 
+cadenas de texto.Deberás imprimir el resultado por la pantalla del navegador y la consola. */ 
+
+/* La única variable de tipo texto es la segunda */
+
+let coder = (nombre) + (apellido);
+console.log(coder);
+document.write(coder);
